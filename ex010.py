15dfb0b9fcf5b874877038510d49f17c0dd8989d
Davi Marcelino <marcelinodavi265@gmail.com>
2026-02-06 08:04:50 -0300

    hi

--- ex010.py
+++ ex010.py
@@ -1,1 +1,3 @@
 n = float(input('Quanto dinheiro você tem na carteira(R$): '))
+n = float(input('Quanto dinheiro você tem na carteira(R$): '))
+n = float(input('Quanto dinheiro você tem na carteira(R$): '))
